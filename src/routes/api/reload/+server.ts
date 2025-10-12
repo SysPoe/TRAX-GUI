4d203a516f1bd9324a71c162e024516f67598114
SysPoe <panddplayz@outlook.com>
2025-10-12 19:26:15 +1000

    return proper response for reload

--- src/routes/api/reload/+server.ts
+++ src/routes/api/reload/+server.ts
@@ -1,5 +1,13 @@
+import { isTRAXLoaded, isTRAXLoading, loadTRAX } from "$lib";
 import TRAX from "translink-rail-api";
 
 export function GET() {
-    TRAX.updateRealtime();
+	if (!isTRAXLoading && isTRAXLoaded) {
+		TRAX.updateRealtime();
+		return new Response("Realtime reload initiated.", { status: 200 });
+	} else if (!isTRAXLoading && !isTRAXLoaded) {
+		loadTRAX();
+        return new Response("Static reload initiated.", { status: 200 });
+    }
+    return new Response("Reload already in progress.", { status: 503 });
 }
