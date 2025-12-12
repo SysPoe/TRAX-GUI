e5fb843e88bac8e85091542db938b4c61a35155a
SysPoe <panddplayz@outlook.com>
2025-12-12 11:30:05 -0600

    Remove unnecessary file

--- check_trax.js
+++
@@ -1,5 +0,0 @@
-import TRAX from "translink-rail-api";
-console.log(Object.keys(TRAX));
-console.log("getRawStopTimes:", typeof TRAX.getRawStopTimes);
-console.log("getRawCalendars:", typeof TRAX.getRawCalendars);
-console.log("getRawCalendarDates:", typeof TRAX.getRawCalendarDates);
